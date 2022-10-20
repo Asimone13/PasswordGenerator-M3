f56fa2c4c1d439359be706e4e68a1d11166d718e
Alec Simone <alecsimone2@gmail.com>
2022-10-20 18:08:18 -0400

    created character sets

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,14 @@
 // Assignment code here
 
+function generatePassword() {
+  // I created each character set separately as an array and depending on the user's answer choices, I will append the set to a blank array that I created.
+  var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+  var selectedArray = [];
 
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
